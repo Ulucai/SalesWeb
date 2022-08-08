20df51d6584b1aac052a63ed606e15ed19a7f645
Ulucai <drodrigoandrade@gmail.com>
2022-08-07 22:41:17 -0400

    Simple Create Form

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -27,6 +27,12 @@ namespace SalesWebMvc.Services
             return _context.Seller.ToList();
         }
 
+        public void Insert (Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges(); // Commit to DB
+        }
+
 
 
         
